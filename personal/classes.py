4eed58d19b6521b47a26721da6fe6f52d54e9798
Josh Gribbon <jgribbon@stevens.edu>
2015-05-01 13:42:52 -0400

    edited classes.py

--- personal/classes.py
+++ personal/classes.py
@@ -1,8 +1,16 @@
 class Dog:
+    kind = 'canine'
 
     def __init__(self, name):
         self.name = name
-        self.tricks = []    # creates a new empty list for each dog
+        self.tricks = []    # creates a new empty list for each dog, must be in init or else it uses one list for all dogs
 
     def add_trick(self, trick):
         self.tricks.append(trick)
+        print "Added '%s' to dog '%s'" %(trick,self.name)
+
+jeff = Dog("Jeff")
+print jeff.kind
+jeff.add_trick("sit")
+jeff.add_trick("lay")
+print jeff.tricks
